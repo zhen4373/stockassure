aae053ee4cfcc21d6e8a2cf1c50c08d56e6d9890
zhen4373 <94926670+zhen4373@users.noreply.github.com>
2026-07-14 07:26:31 +0800

    Update config.py

--- core/config.py
+++ core/config.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings:
     PROJECT_NAME: str = "StockAssure"
-    VERSION: str = "0.1.5"
+    VERSION: str = "0.5.0"
     DB_DIR: Path = BASE_DIR / "config"
     DATABASE_URL: str = f"sqlite:///{DB_DIR}/database.db"
     PLUGINS_DIR: Path = BASE_DIR / "plugins"
